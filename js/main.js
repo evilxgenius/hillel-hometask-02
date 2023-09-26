94a7e5e0c6a79afeb0ffc657b54aaf3515ee1ae0
Sergey E <evilx.emelyanov@gmail.com>
2023-09-26 23:31:44 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNum = Number(prompt('enter number #1:'));
+let secondNum = Number(prompt('enter number #2:'));
+
+alert(`User entered ${firstNum} and ${secondNum}:
+    ${firstNum} + ${secondNum} = ${firstNum + secondNum}
+    ${firstNum} - ${secondNum} = ${firstNum - secondNum}
+    ${firstNum} * ${secondNum} = ${firstNum * secondNum}
+    ${firstNum} / ${secondNum} = ${firstNum / secondNum}`
+);
